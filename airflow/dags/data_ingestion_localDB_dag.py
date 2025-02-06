18f28e31b3c82482b402aadb0e2ed151050b0561
Jordan <27322284+jpayrun@users.noreply.github.com>
2025-02-05 21:19:18 -0500

    Initial Checkin

---
+++ airflow/dags/data_ingestion_localDB_dag.py
@@ -0,0 +1,50 @@
+import os
+from datetime import datetime
+
+import pyarrow
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2025,2,5),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+local_workflow = DAG(
+    dag_id="LocalIngestionDag",
+    schedule_interval="0 6 2 * *",
+    default_args=default_args
+)
+
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow")
+
+def hello():
+    print("Hello World!")
+    return "I Ran!"
+
+url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet"
+
+with local_workflow:
+
+    wget_task = BashOperator(
+        task_id = 'wget',
+        bash_command = f'wget {url} -O {AIRFLOW_HOME}/output.parquet'
+    )
+
+    # ingest_task = PythonOperator(
+    #     task_id='ingest',
+    #     python_callable=hello
+    # )
+
+    ingest_task = BashOperator(
+        task_id='ingest',
+        bash_command=f'ls {AIRFLOW_HOME}'
+    )
+
+    wget_task >> ingest_task
