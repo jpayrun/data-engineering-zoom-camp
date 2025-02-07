54feae26c9f56ad82d3b32a01eb853ebabf5c6e5
Jordan <27322284+jpayrun@users.noreply.github.com>
2025-02-06 22:17:32 -0500

    Updated code for airflow

--- airflow/dags/ingest_script.py
+++ airflow/dags/ingest_script.py
@@ -6,12 +6,12 @@ import pandas as pd
 import pyarrow
 import psycopg2
 
-def ingest_callable(table_name, password, host, user, port, db):
-    csv = "output.csv"
+def ingest_callable(table_name, password, host, user, port, db, csv_file):
 
-    engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}')
-    engine.connect()
+    constr = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}'
+    # print(constr)
+    con = create_engine(constr)
 
-    # df = pd.read_parquet(csv)
-    # df.head(0).to_sql(name = table_name, con=con, if_exists='replace')
-    # df.to_sql(name=table_name, con=con, if_exists='append')
+    df = pd.read_parquet(csv_file)
+    df.head(0).to_sql(name = table_name, con=con, if_exists='replace')
+    df.to_sql(name=table_name, con=con, if_exists='append')
